76421516085736c8d64542f182cea25371f89b1f
Baptiste Langlade <baptouuuu@gmail.com>
2015-07-27 08:56:37 +0200

    add listener to extract id from entity

---
+++ Tests/EventListener/StorageCreateListenerTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Innmind\Rest\Server\Tests\EventListener;
+
+use Innmind\Rest\Server\EventListener\StorageCreateListener;
+use Innmind\Rest\Server\Resource;
+use Innmind\Rest\Server\Definition\Resource as Definition;
+use Innmind\Rest\Server\Event\Storage\PostCreateEvent;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+class StorageCreateListenerTest extends \PHPUnit_Framework_TestCase
+{
+    protected $l;
+    protected $r;
+
+    public function setUp()
+    {
+        $this->l = new StorageCreateListener(
+            PropertyAccess::createPropertyAccessor()
+        );
+        $this->r = new Resource;
+        $this->r->setDefinition(
+            (new Definition('foo'))
+                ->setId('id')
+        );
+    }
+
+    public function testExtractIdAccessibleByGetter()
+    {
+        $e = new PostCreateEvent($this->r, new Foo);
+        $this->assertFalse($e->hasResourceId());
+        $this->l->extractId($e);
+        $this->assertTrue($e->hasResourceId());
+        $this->assertSame(
+            42,
+            $e->getResourceId()
+        );
+    }
+
+    public function testExtractProtectedId()
+    {
+        $e = new PostCreateEvent($this->r, new Bar);
+        $this->assertFalse($e->hasResourceId());
+        $this->l->extractId($e);
+        $this->assertTrue($e->hasResourceId());
+        $this->assertSame(
+            24,
+            $e->getResourceId()
+        );
+    }
+}
+
+class Foo {
+    protected $id;
+
+    public function getId()
+    {
+        return 42;
+    }
+}
+
+class Bar {
+    protected $id = 24;
+}
