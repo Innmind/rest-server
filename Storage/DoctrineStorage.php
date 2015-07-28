7b317af037290aa33a502d4a5df0bc575be66c82
Baptiste Langlade <baptouuuu@gmail.com>
2015-07-28 08:16:22 +0200

    add doctrine storage

---
+++ Storage/DoctrineStorage.php
@@ -0,0 +1,224 @@
+<?php
+
+namespace Innmind\Rest\Server\Storage;
+
+use Innmind\Rest\Server\StorageInterface;
+use Innmind\Rest\Server\Resource;
+use Innmind\Rest\Server\ResourceBuilder;
+use Innmind\Rest\Server\Events;
+use Innmind\Rest\Server\Event\Storage;
+use Innmind\Rest\Server\Event\Doctrine\ReadQueryBuilderEvent;
+use Innmind\Rest\Server\Definition\Resource as ResourceDefinition;
+use Innmind\Rest\Server\Exception\ResourceNotSupportedException;
+use Innmind\Rest\Server\EntityBuilder;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Common\Collections\Collection;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class DoctrineStorage implements StorageInterface
+{
+    protected $em;
+    protected $dispatcher;
+    protected $entityBuilder;
+    protected $resourceBuilder;
+
+    public function __construct(
+        EntityManagerInterface $em,
+        EventDispatcherInterface $dispatcher,
+        EntityBuilder $entityBuilder,
+        ResourceBuilder $resourceBuilder
+    ) {
+        $this->em = $em;
+        $this->dispatcher = $dispatcher;
+        $this->entityBuilder = $entityBuilder;
+        $this->resourceBuilder = $resourceBuilder;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read(ResourceDefinition $definition, $id = null)
+    {
+        $this->dispatcher->dispatch(
+            Events::STORAGE_PRE_READ,
+            $event = new Storage\PreReadEvent($definition, $id)
+        );
+
+        if ($event->hasResources()) {
+            return $event->getResources();
+        }
+
+        $this->checkSupport($definition);
+
+        $qb = $this->em
+            ->createQueryBuilder()
+            ->select('r')
+            ->from($definition->getOption('class'), 'r');
+
+        if ($id !== null) {
+            $qb
+                ->where(sprintf(
+                    'r.%s = :id',
+                    $definition->getId()
+                ))
+                ->setParameter('id', $id);
+        }
+
+        $this->dispatcher->dispatch(
+            Events::DOCTRINE_READ_QUERY_BUILDER,
+            $event = new ReadQueryBuilderEvent($definition, $id, $qb)
+        );
+
+        if ($event->hasResources()) {
+            return $event->getResources();
+        }
+
+        $entities = $event->getQueryBuilder()->getQuery()->getResult();
+        $entities = $entities instanceof Collection ?
+            $entities->toArray() :
+            (array) $entities;
+        $resources = new \SplObjectStorage;
+
+        foreach ($entities as $entity) {
+            $resources->attach(
+                $this->resourceBuilder->build($entity, $definition)
+            );
+        }
+
+        $this->dispatcher->dispatch(
+            Events::STORAGE_POST_READ,
+            $event = new Storage\PostReadEvent($definition, $id, $resources)
+        );
+
+        $resources = $event->getResources();
+        $resources->rewind();
+
+        return $resources;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function create(Resource $resource)
+    {
+        $this->dispatcher->dispatch(
+            Events::STORAGE_PRE_CREATE,
+            $event = new Storage\PreCreateEvent($resource)
+        );
+
+        if ($event->hasResourceId()) {
+            return $event->getResourceId();
+        }
+
+        $this->checkSupport($event->getResource()->getDefinition());
+
+        $entity = $this->entityBuilder->build($event->getResource());
+
+        $this->em->persist($entity);
+        $this->em->flush();
+
+        $this->dispatcher->dispatch(
+            Events::STORAGE_POST_CREATE,
+            $event = new Storage\PostCreateEvent($event->getResource(), $entity)
+        );
+
+        return $event->getResourceId();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function update(Resource $resource, $id)
+    {
+        $this->dispatcher->dispatch(
+            Events::STORAGE_PRE_UPDATE,
+            $event = new Storage\PreUpdateEvent($resource, $id)
+        );
+
+        if ($event->isPropagationStopped()) {
+            return $this;
+        }
+
+        $resource = $event->getResource();
+        $this->checkSupport($resource->getDefinition());
+
+        $entity = $this->em->find(
+            $resource->getDefinition()->getOption('class'),
+            $id
+        );
+        $this->entityBuilder->build($resource, $entity);
+
+        $this->em->flush();
+
+        $this->dispatcher->dispatch(
+            Events::STORAGE_POST_UPDATE,
+            new Storage\PostUpdateEvent($resource, $id, $entity)
+        );
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete(ResourceDefinition $definition, $id)
+    {
+        $this->dispatcher->dispatch(
+            Events::STORAGE_PRE_DELETE,
+            $event = new Storage\PreDeleteEvent($definition, $id)
+        );
+
+        if ($event->isPropagationStopped()) {
+            return $this;
+        }
+
+        $this->checkSupport($definition);
+
+        $entity = $this->em->find($definition->getOption('class'), $id);
+        $this->em->remove($entity);
+        $this->em->flush();
+
+        $this->dispatcher->dispatch(
+            Events::STORAGE_POST_DELETE,
+            new Storage\PostDeleteEvent($definition, $id, $entity)
+        );
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports(ResourceDefinition $definition)
+    {
+        if (!$definition->hasOption('class')) {
+            return false;
+        }
+
+        $class = $definition->getOption('class');
+
+        if (!$this->em->getMetadataFactory()->hasMetadataFor($class)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Verify if the doctrine storage can allow this resource definition
+     *
+     * @param ResourceDefinition $definition
+     *
+     * @throws ResourceNotSupportedException In case the storage can't
+     */
+    protected function checkSupport(ResourceDefinition $definition)
+    {
+        if (!$this->supports($definition)) {
+            throw new ResourceNotSupportedException(sprintf(
+                'Doctrine can\'t support the resource %s::%s',
+                $definition->getCollection(),
+                $definition
+            ));
+        }
+    }
+}
---
+++ Tests/Storage/DoctrineStorageTest.php
@@ -0,0 +1,456 @@
+<?php
+
+namespace Innmind\Rest\Server\Tests\Storage;
+
+use Innmind\Rest\Server\Storage\DoctrineStorage;
+use Innmind\Rest\Server\EntityBuilder;
+use Innmind\Rest\Server\ResourceBuilder;
+use Innmind\Rest\Server\Resource;
+use Innmind\Rest\Server\Definition\Resource as ResourceDefinition;
+use Innmind\Rest\Server\Definition\Property;
+use Innmind\Rest\Server\Event\Storage;
+use Innmind\Rest\Server\Event\Doctrine\ReadQueryBuilderEvent;
+use Innmind\Rest\Server\EventListener\StorageCreateListener;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\SchemaTool;
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\Mapping\Driver\SimplifiedYamlDriver;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Common\Collections\ArrayCollection;
+
+class DoctrineStorageTest extends \PHPUnit_Framework_TestCase
+{
+    protected $s;
+    protected $em;
+    protected $d;
+    protected $eb;
+    protected $rb;
+    protected $def;
+
+    public function setUp()
+    {
+        $conf = new Configuration;
+        $conf->setMetadataDriverImpl(new SimplifiedYamlDriver([
+            'fixtures/doctrine' => 'Innmind\Rest\Server\Tests\Storage',
+        ]));
+        $conf->setProxyDir(sys_get_temp_dir());
+        $conf->setProxyNamespace('Doctrine\__PROXY__');
+        $this->em = EntityManager::create(
+            [
+                'driver' => 'pdo_sqlite',
+                'dbname' => ':memory:',
+            ],
+            $conf
+        );
+
+        $tool = new SchemaTool($this->em);
+        $tool->updateSchema([
+            $this->em->getClassMetadata(Foo::class),
+        ]);
+
+        $this->s = new DoctrineStorage(
+            $this->em,
+            $this->d = new EventDispatcher,
+            $this->eb = new EntityBuilder(
+                $accessor = PropertyAccess::createPropertyAccessor(),
+                $this->d
+            ),
+            $this->rb = new ResourceBuilder(
+                $accessor,
+                Validation::createValidator(),
+                $this->d
+            )
+        );
+
+        $this->def = new ResourceDefinition('foo');
+        $this->def
+            ->setId('id')
+            ->addProperty(
+                (new Property('id'))
+                    ->setType('int')
+            )
+            ->addProperty(
+                (new Property('name'))
+                    ->setType('string')
+            )
+            ->addOption('class',  Foo::class);
+        $this->d->addSubscriber(new StorageCreateListener(
+            PropertyAccess::createPropertyAccessor()
+        ));
+    }
+
+    public function testSupports()
+    {
+        $this->assertTrue($this->s->supports($this->def));
+    }
+
+    public function testDoesnSupport()
+    {
+        $def = clone $this->def;
+        $def->addOption('class', self::class);
+        $this->assertFalse($this->s->supports($def));
+    }
+
+    public function testCreateResourceInPreEvent()
+    {
+        $this->d->addListener(
+            'innmind.rest.storage.pre.create',
+            function(Storage\PreCreateEvent $event) {
+                $event->setResourceId(42);
+            }
+        );
+        $r = new Resource;
+        $r
+            ->setDefinition($this->def)
+            ->set('name', 'foo');
+        $id = $this->s->create($r);
+
+        $this->assertSame(42, $id);
+    }
+
+    public function testDispatchPostCreate()
+    {
+        $fired = false;
+        $this->d->addListener(
+            'innmind.rest.storage.post.create',
+            function(Storage\PostCreateEvent $event) use (&$fired) {
+                $fired = true;
+                $this->assertInstanceOf(
+                    Foo::class,
+                    $event->getEntity()
+                );
+            },
+            10
+        );
+        $r = new Resource;
+        $r
+            ->setDefinition($this->def)
+            ->set('name', 'foo');
+        $id = $this->s->create($r);
+
+        $this->assertTrue($fired);
+    }
+
+    public function testCreate()
+    {
+        $r = new Resource;
+        $r
+            ->setDefinition($this->def)
+            ->set('name', 'foo');
+        $id = $this->s->create($r);
+
+        $this->assertTrue(is_int($id));
+    }
+
+    public function testDispatchPreRead()
+    {
+        $fired = false;
+        $this->d->addListener(
+            'innmind.rest.storage.pre.read',
+            function(Storage\PreReadEvent $event) use (&$fired) {
+                $fired = true;
+                $event->addResource(
+                    (new Resource)
+                        ->setDefinition($this->def)
+                );
+            }
+        );
+        $resources = $this->s->read($this->def);
+
+        $this->assertTrue($fired);
+        $this->assertSame(1, $resources->count());
+    }
+
+    public function testDispatchDoctrineQB()
+    {
+        $fired = false;
+        $this->d->addListener(
+            'innmind.rest.storage.doctrine.read_query_builder',
+            function(ReadQueryBuilderEvent $event) use (&$fired) {
+                $fired = true;
+                $this->assertInstanceOf(
+                    QueryBuilder::class,
+                    $event->getQueryBuilder()
+                );
+            }
+        );
+        $resources = $this->s->read($this->def);
+
+        $this->assertTrue($fired);
+    }
+
+    public function testDispatchPostRead()
+    {
+        $fired = false;
+        $this->d->addListener(
+            'innmind.rest.storage.post.read',
+            function(Storage\PostReadEvent $event) use (&$fired) {
+                $fired = true;
+                $event->addResource(
+                    (new Resource)
+                        ->setDefinition($this->def)
+                );
+            }
+        );
+        $resources = $this->s->read($this->def);
+
+        $this->assertTrue($fired);
+        $this->assertSame(1, $resources->count());
+    }
+
+    public function testRead()
+    {
+        $r = new Resource;
+        $r
+            ->setDefinition($this->def)
+            ->set('name', 'foo');
+        $id = $this->s->create($r);
+        $this->s->create(clone $r);
+
+        $resources = $this->s->read($this->def);
+
+        $this->assertInstanceOf(
+            \SplObjectStorage::class,
+            $resources
+        );
+        $this->assertSame(
+            2,
+            $resources->count()
+        );
+
+        $resource = $this->s->read($this->def, $id);
+
+        $this->assertSame(
+            1,
+            $resource->count()
+        );
+        $this->assertInstanceOf(
+            Resource::class,
+            $resource->current()
+        );
+        $this->assertSame(
+            $id,
+            $resource->current()->get('id')
+        );
+        $this->assertSame(
+            'foo',
+            $resource->current()->get('name')
+        );
+        $this->assertSame(
+            $this->def,
+            $resource->current()->getDefinition()
+        );
+    }
+
+    public function testDispatchPreUpdate()
+    {
+        $r = new Resource;
+        $r
+            ->setDefinition($this->def)
+            ->set('name', 'foo');
+        $id = $this->s->create($r);
+
+        $fired = false;
+        $this->d->addListener(
+            'innmind.rest.storage.pre.update',
+            function(Storage\PreUpdateEvent $event) use (&$fired, $r, $id) {
+                $fired = true;
+                $this->assertSame(
+                    $r,
+                    $event->getResource()
+                );
+                $this->assertSame(
+                    $id,
+                    $event->getResourceId()
+                );
+            }
+        );
+        $r->set('name', 'bar');
+        $this->assertSame(
+            $this->s,
+            $this->s->update($r, $id)
+        );
+        $this->assertTrue($fired);
+        $resource = $this->s->read($this->def, $id);
+        $this->assertSame(
+            'bar',
+            $resource->current()->get('name')
+        );
+    }
+
+    public function testPreventUpdate()
+    {
+        $fired = false;
+        $this->d->addListener(
+            'innmind.rest.storage.post.update',
+            function(Storage\PostUpdateEvent $event) use (&$fired) {
+                $fired = true;
+            }
+        );
+        $this->d->addListener(
+            'innmind.rest.storage.pre.update',
+            function(Storage\PreUpdateEvent $event) use (&$fired) {
+                $event->stopPropagation();
+            }
+        );
+
+        $r = new Resource;
+        $r
+            ->setDefinition($this->def)
+            ->set('name', 'foo');
+        $id = $this->s->create($r);
+        $r->set('name', 'bar');
+        $this->assertSame(
+            $this->s,
+            $this->s->update($r, $id)
+        );
+        $this->assertFalse($fired);
+    }
+
+    public function testDispatchPostUpdate()
+    {
+        $r = new Resource;
+        $r
+            ->setDefinition($this->def)
+            ->set('name', 'foo');
+        $id = $this->s->create($r);
+        $fired = false;
+        $this->d->addListener(
+            'innmind.rest.storage.post.update',
+            function(Storage\PostUpdateEvent $event) use (&$fired, $r, $id) {
+                $fired = true;
+                $this->assertSame(
+                    $r,
+                    $event->getResource()
+                );
+                $this->assertSame(
+                    $id,
+                    $event->getResourceId()
+                );
+                $this->assertInstanceOf(
+                    Foo::class,
+                    $event->getEntity()
+                );
+            }
+        );
+
+        $r->set('name', 'bar');
+        $this->assertSame(
+            $this->s,
+            $this->s->update($r, $id)
+        );
+        $this->assertTrue($fired);
+    }
+
+    public function testDispatchPreDelete()
+    {
+        $r = new Resource;
+        $r
+            ->setDefinition($this->def)
+            ->set('name', 'foo');
+        $id = $this->s->create($r);
+        $fired = false;
+        $this->d->addListener(
+            'innmind.rest.storage.pre.delete',
+            function(Storage\PreDeleteEvent $event) use (&$fired, $r, $id) {
+                $fired = true;
+                $this->assertSame(
+                    $r->getDefinition(),
+                    $event->getDefinition()
+                );
+                $this->assertSame(
+                    $id,
+                    $event->getResourceId()
+                );
+                $event->stopPropagation();
+            }
+        );
+        $postFired = false;
+        $this->d->addListener(
+            'innmind.rest.storage.post.delete',
+            function(Storage\POstDeleteEvent $event) use (&$postFired) {
+                $postFired = true;
+            }
+        );
+
+        $this->assertSame(
+            $this->s,
+            $this->s->delete($this->def, $id)
+        );
+        $this->assertTrue($fired);
+        $this->assertFalse($postFired);
+    }
+
+    public function testDispatchPostDelete()
+    {
+        $r = new Resource;
+        $r
+            ->setDefinition($this->def)
+            ->set('name', 'foo');
+        $id = $this->s->create($r);
+        $fired = false;
+        $this->d->addListener(
+            'innmind.rest.storage.post.delete',
+            function(Storage\PostDeleteEvent $event) use (&$fired, $r, $id) {
+                $fired = true;
+                $this->assertSame(
+                    $r->getDefinition(),
+                    $event->getDefinition()
+                );
+                $this->assertSame(
+                    $id,
+                    $event->getResourceId()
+                );
+                $this->assertInstanceOf(
+                    Foo::class,
+                    $event->getEntity()
+                );
+            }
+        );
+
+        $this->assertSame(
+            $this->s,
+            $this->s->delete($this->def, $id)
+        );
+        $this->assertTrue($fired);
+    }
+
+    public function testDelete()
+    {
+        $r = new Resource;
+        $r
+            ->setDefinition($this->def)
+            ->set('name', 'foo');
+        $id = $this->s->create($r);
+
+        $this->assertSame(
+            $this->s,
+            $this->s->delete($this->def, $id)
+        );
+        $this->assertSame(
+            0,
+            $this->s->read($this->def)->count()
+        );
+    }
+}
+
+/**
+ * @ORM\Entity
+ */
+class Foo {
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    public $id;
+
+    /**
+     * @ORM\Column(name="name", type="string")
+     */
+    public $name;
+}
