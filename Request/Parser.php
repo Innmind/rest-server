5accb36c8f786fee789989d5d0127bf830e8807a
Baptiste Langlade <baptouuuu@gmail.com>
2015-08-05 09:12:01 +0200

    add class to parse http request to extract resource(s) data

---
+++ Request/Parser.php
@@ -0,0 +1,228 @@
+<?php
+
+namespace Innmind\Rest\Server\Request;
+
+use Innmind\Rest\Server\Definition\Resource as ResourceDefinition;
+use Innmind\Rest\Server\Formats;
+use Innmind\Rest\Server\Exception\PayloadException;
+use Symfony\Component\HttpFoundation\Request as HttpRequest;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\Exception\ExceptionInterface;
+use Negotiation\Negotiator;
+
+class Parser
+{
+    protected $serializer;
+    protected $formats;
+    protected $negotiator;
+
+    public function __construct(
+        Serializer $serializer,
+        Formats $formats,
+        Negotiator $negotiator
+    ) {
+        $this->serializer = $serializer;
+        $this->formats = $formats;
+        $this->negotiator = $negotiator;
+    }
+
+    /**
+     * Check if the request content type is known
+     *
+     * @param HttpRequest $request
+     *
+     * @return bool
+     */
+    public function isContentTypeAcceptable(HttpRequest $request)
+    {
+        $contentType = $request->headers->get('Content-Type');
+
+        if (!$this->formats->has($contentType)) {
+            return false;
+        }
+
+        $format = $this->formats->getName($contentType);
+
+        if (!$this->serializer->supportsDecoding($format)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Check if the wished accepted types for the client are supported
+     *
+     * @param HttpRequest $request
+     *
+     * @return bool
+     */
+    public function isRequestedTypeAcceptable(HttpRequest $request)
+    {
+        $wished = $this->negotiator->getBest(
+            $request->headers->get('Accept'),
+            $this->formats->getMediaTypes()
+        );
+
+        if ($wished === null) {
+            return false;
+        }
+
+        $format = $this->formats->getName($wished->getValue());
+
+        return $this->serializer->supportsEncoding($format);
+    }
+
+    /**
+     * Return the data payload as an object
+     *
+     * @param HttpRequest $request
+     * @param ResourceDefinition $definition
+     *
+     * @return object|array
+     */
+    public function getData(
+        HttpRequest $request,
+        ResourceDefinition $definition
+    ) {
+        $format = $this->formats->getName(
+            $request->headers->get('Content-Type')
+        );
+        $payload = $this->serializer->decode($request, $format);
+
+        if (isset($payload['resource'])) {
+            return $this->computePayload($payload['resource'], $definition);
+        } else if (isset($payload['resources'])) {
+            $objects = [];
+
+            foreach ($payload['resources'] as $object) {
+                $objects[] = $this->computePayload($object, $definition);
+            }
+
+            return $objects;
+        }
+
+        throw new PayloadException(
+            'The payload must be set either under ' .
+            'the key "resource" or "resources"'
+        );
+    }
+
+    /**
+     * Tranform the array payload into an object
+     * and validate it via an option resolver
+     *
+     * @param array $payload
+     * @param ResourceDefinition $definition
+     *
+     * @throws PayloadException If a field is unknown or misconfigured
+     *
+     * @return stdClass
+     */
+    protected function computePayload(
+        array $payload,
+        ResourceDefinition $definition
+    ) {
+        try {
+            $this->validatePayload($payload, $definition);
+        } catch (ExceptionInterface $e) {
+            throw new PayloadException('Bad request payload', 0, $e);
+        }
+
+        return $this->transformArrayToObject($payload, $definition);
+    }
+
+    /**
+     * Make sure the payload match the resource definition
+     *
+     * @param array $payload
+     * @param ResourceDefinition $definition
+     *
+     * @return void
+     */
+    protected function validatePayload(
+        array $payload,
+        ResourceDefinition $definition
+    ) {
+        $resolver = new OptionsResolver;
+
+        foreach ($definition->getProperties() as $property) {
+            $resolver->setRequired((string) $property);
+
+            if ($property->getType() === 'resource') {
+                $resolver->addAllowedTypes((string) $property, 'array');
+            } else {
+                $resolver->addAllowedTypes(
+                    (string) $property,
+                    $property->getType()
+                );
+            }
+        }
+
+        $payload = $resolver->resolve($payload);
+
+        foreach ($definition->getProperties() as $property) {
+            if ($property->getType() === 'resource') {
+                $this->validatePayload(
+                    $payload[(string) $property],
+                    $property->getOption('resource')
+                );
+            } else if (
+                $property->getType() === 'array' &&
+                $property->getOption('inner_type') === 'resource'
+            ) {
+                foreach ($payload[(string) $property] as $subValue) {
+                    $this->validatePayload(
+                        $subValue,
+                        $property->getOption('resource')
+                    );
+                }
+            }
+        }
+    }
+
+    /**
+     * Transform an array to an object
+     *
+     * @param array $data
+     * @param ResourceDefinition $definition
+     *
+     * @return stdClass
+     */
+    protected function transformArrayToObject(
+        array $data,
+        ResourceDefinition $definition
+    ) {
+        $o = new \stdClass;
+
+        foreach ($data as $key => $value) {
+            $property = $definition->getProperty($key);
+
+            if (
+                $property->getType() === 'array' &&
+                $property->getOption('inner_type') === 'resource'
+            ) {
+                $collection = [];
+
+                foreach ($value as $subValue) {
+                    $collection[] = $this->transformArrayToObject(
+                        $subValue,
+                        $property->getOption('resource')
+                    );
+                }
+
+                $o->$key = $collection;
+            } else if ($property->getType() === 'resource') {
+                $o->$key = $this->transformArrayToObject(
+                    $value,
+                    $property->getOption('resource')
+                );
+            } else {
+                $o->$key = $value;
+            }
+        }
+
+        return $o;
+    }
+}
---
+++ Tests/Request/ParserTest.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace Innmind\Rest\Server\Tests\Request;
+
+use Innmind\Rest\Server\Request\Parser;
+use Innmind\Rest\Server\Formats;
+use Innmind\Rest\Server\Definition\Resource as Definition;
+use Innmind\Rest\Server\Definition\Property;
+use Innmind\Rest\Server\Serializer\Encoder\FormEncoder;
+use Innmind\Rest\Server\Serializer\Encoder\JsonEncoder;
+use Innmind\Rest\Server\Serializer\Normalizer\ResourceNormalizer;
+use Negotiation\Negotiator;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\HttpFoundation\Request as HttpRequest;
+
+class ParserTest extends \PHPUnit_Framework_TestCase
+{
+    protected $p;
+
+    public function setUp()
+    {
+        $serializer = new Serializer(
+            [new ResourceNormalizer],
+            [new FormEncoder, new JsonEncoder]
+        );
+        $formats = new Formats;
+        $formats
+            ->add('json', 'application/json', 10)
+            ->add('form', 'application/x-www-form-urlencoded', 1);
+
+        $this->p = new Parser(
+            $serializer,
+            $formats,
+            new Negotiator
+        );
+    }
+
+    public function testIsContentTypeAcceptable()
+    {
+        $r = new HttpRequest;
+        $r->headers->add(['Content-Type' => 'application/json']);
+
+        $this->assertTrue($this->p->isContentTypeAcceptable($r));
+    }
+
+    public function testIsContentTypeNotAcceptable()
+    {
+        $r = new HttpRequest;
+        $r->headers->add(['Content-Type' => 'text/json']);
+
+        $this->assertFalse($this->p->isContentTypeAcceptable($r));
+    }
+
+    public function testIsRequestedTypeAcceptable()
+    {
+        $r = new HttpRequest;
+        $r->headers->add(['Accept' => 'application/json']);
+
+        $this->assertTrue($this->p->isRequestedTypeAcceptable($r));
+    }
+
+    public function testIsRequestedTypeNotAcceptable()
+    {
+        $r = new HttpRequest;
+        $r->headers->add(['Accept' => 'text/json']);
+
+        $this->assertFalse($this->p->isRequestedTypeAcceptable($r));
+    }
+
+    public function testGetData()
+    {
+        $data = [
+            'foo' => 'bar',
+            'sub' => [
+                'foo' => 'bar',
+            ],
+            'subColl' => [[
+                'foo' => 'bar',
+            ]],
+        ];
+        $expected = new \stdClass;
+        $sub = new \stdClass;
+        $sub->foo = 'bar';
+        $expected->foo = 'bar';
+        $expected->sub = $sub;
+        $expected->subColl = [$sub];
+        $subDef = (new Definition('sub'))
+            ->addProperty(
+                (new Property('foo'))
+                    ->setType('string')
+            );
+        $definition = new Definition('foo');
+        $definition
+            ->addProperty(
+                (new Property('foo'))
+                    ->setType('string')
+            )
+            ->addProperty(
+                (new Property('sub'))
+                    ->setType('resource')
+                    ->addOption('resource', $subDef)
+            )
+            ->addProperty(
+                (new Property('subColl'))
+                    ->setType('array')
+                    ->addOption('inner_type', 'resource')
+                    ->addOption('resource', $subDef)
+            );
+        $r = new HttpRequest(
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            json_encode(['resource' => $data])
+        );
+        $r->headers->add(['Content-Type' => 'application/json']);
+
+        $this->assertEquals(
+            $expected,
+            $this->p->getData($r, $definition)
+        );
+    }
+
+    /**
+     * @expectedException Innmind\Rest\Server\Exception\PayloadException
+     * @expectedExceptionMessage Bad request payload
+     */
+    public function testThrowIfPayloadDoesntMatchDefinition()
+    {
+        $definition = new Definition('foo');
+        $definition
+            ->addProperty(
+                (new Property('foo'))
+                    ->setType('int')
+            )
+            ->addProperty(
+                (new Property('bar'))
+                    ->setType('string')
+            );
+        $r = new HttpRequest(
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            '{"resource":{"foo":"42"}}'
+        );
+        $r->headers->add(['Content-Type' => 'application/json']);
+
+        $this->p->getData($r, $definition);
+    }
+
+    public function testGetCollectionOfData()
+    {
+        $def = new Definition('foo');
+        $def->addProperty(
+            (new Property('foo'))
+                ->setType('string')
+        );
+        $data = [
+            ['foo' => 'bar'],
+            ['foo' => 'baz'],
+        ];
+        $r = new HttpRequest(
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            json_encode(['resources' => $data])
+        );
+        $r->headers->add(['Content-Type' => 'application/json']);
+        $expected = [];
+        $o = new \stdClass;
+        $o->foo = 'bar';
+        $expected[] = $o;
+        $o = new \stdClass;
+        $o->foo = 'baz';
+        $expected[] = $o;
+
+        $this->assertEquals(
+            $expected,
+            $this->p->getData($r, $def)
+        );
+    }
+
+    /**
+     * @expectedException Innmind\Rest\Server\Exception\PayloadException
+     * @expectedExceptionMessage The payload must be set either under the key "resource" or "resources"
+     */
+    public function testThrowIfNoResourceKeyInPayload()
+    {
+        $definition = new Definition('foo');
+        $definition->addProperty(
+            (new Property('foo'))
+                ->setType('int')
+        );
+        $r = new HttpRequest(
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            '{"foo":42}'
+        );
+        $r->headers->add(['Content-Type' => 'application/json']);
+
+        $this->p->getData($r, $definition);
+    }
+}
