5186fd465b1723476f54c266240201d1dbf64963
Baptiste Langlade <baptouuuu@gmail.com>
2015-08-26 09:02:08 +0200

    add method to expose the collections node config

--- Configuration.php
+++ Configuration.php
@@ -17,54 +17,68 @@ class Configuration implements ConfigurationInterface
 
         $root
             ->children()
-                ->arrayNode('collections')
-                    ->useAttributeAsKey('name')
-                    ->requiresAtLeastOneElement()
-                    ->prototype('array')
-                        ->children()
-                            ->scalarNode('storage')
-                                ->isRequired()
-                            ->end()
-                            ->arrayNode('resources')
-                                ->useAttributeAsKey('name')
-                                ->requiresAtLeastOneElement()
-                                ->prototype('array')
-                                    ->children()
-                                        ->scalarNode('id')
-                                            ->isRequired()
-                                        ->end()
-                                        ->scalarNode('storage')
-                                            ->isRequired()
-                                        ->end()
-                                        ->arrayNode('properties')
-                                            ->useAttributeAsKey('name')
-                                            ->requiresAtLeastOneElement()
-                                            ->prototype('array')
-                                                ->children()
-                                                    ->scalarNode('type')
-                                                        ->isRequired()
-                                                    ->end()
-                                                    ->arrayNode('access')
-                                                        ->isRequired()
-                                                        ->prototype('scalar')->end()
-                                                    ->end()
-                                                    ->arrayNode('variants')
-                                                        ->defaultValue([])
-                                                        ->prototype('scalar')->end()
-                                                    ->end()
-                                                    ->arrayNode('options')
-                                                        ->prototype('variable')->end()
-                                                    ->end()
-                                                ->end()
+                ->append($this->getCollectionNode())
+            ->end();
+
+        return $builder;
+    }
+
+    /**
+     * Return the node to specify the resources collections
+     *
+     * @return NodeInterface
+     */
+    public function getCollectionNode()
+    {
+        $builder = new TreeBuilder;
+        $root = $builder->root('collections');
+
+        $root
+            ->requiresAtLeastOneElement()
+            ->useAttributeAsKey('name')
+            ->prototype('array')
+                ->children()
+                    ->scalarNode('storage')
+                        ->isRequired()
+                    ->end()
+                    ->arrayNode('resources')
+                        ->useAttributeAsKey('name')
+                        ->requiresAtLeastOneElement()
+                        ->prototype('array')
+                            ->children()
+                                ->scalarNode('id')
+                                    ->isRequired()
+                                ->end()
+                                ->scalarNode('storage')
+                                    ->isRequired()
+                                ->end()
+                                ->arrayNode('properties')
+                                    ->useAttributeAsKey('name')
+                                    ->requiresAtLeastOneElement()
+                                    ->prototype('array')
+                                        ->children()
+                                            ->scalarNode('type')
+                                                ->isRequired()
+                                            ->end()
+                                            ->arrayNode('access')
+                                                ->isRequired()
+                                                ->prototype('scalar')->end()
+                                            ->end()
+                                            ->arrayNode('variants')
+                                                ->defaultValue([])
+                                                ->prototype('scalar')->end()
+                                            ->end()
+                                            ->arrayNode('options')
+                                                ->prototype('variable')->end()
                                             ->end()
                                         ->end()
-                                        ->arrayNode('meta')
-                                            ->prototype('variable')->end()
-                                        ->end()
-                                        ->arrayNode('options')
-                                            ->prototype('variable')->end()
-                                        ->end()
                                     ->end()
+                                ->end()
+                                ->arrayNode('meta')
+                                    ->prototype('variable')->end()
+                                ->end()
+                                ->arrayNode('options')
+                                    ->prototype('variable')->end()
                                 ->end()
                             ->end()
                         ->end()
@@ -72,6 +86,6 @@ class Configuration implements ConfigurationInterface
                 ->end()
             ->end();
 
-        return $builder;
+        return $root;
     }
 }
