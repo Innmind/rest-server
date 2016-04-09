34e1fe1e43fe24c863ae3659cf691a98c8ed44a3
Baptiste Langlade <baptouuuu@gmail.com>
2016-04-09 16:26:11 +0200

    add float type

---
+++ Definition/Type/FloatType.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Rest\Server\Definition\Type;
+
+use Innmind\Rest\Server\{
+    Definition\TypeInterface,
+    Exception\DenormalizationException,
+    Exception\NormalizationException
+};
+use Innmind\Immutable\{
+    CollectionInterface,
+    SetInterface,
+    Set
+};
+
+class FloatType implements TypeInterface
+{
+    private static $identifiers;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function fromConfig(CollectionInterface $config): TypeInterface
+    {
+        return new self;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function denormalize($data)
+    {
+        if (!is_float($data)) {
+            throw new DenormalizationException('The value must be a float');
+        }
+
+        return $data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize($data)
+    {
+        if (!is_float($data)) {
+            throw new NormalizationException('The value must be a float');
+        }
+
+        return $data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function identifiers(): SetInterface
+    {
+        if (self::$identifiers === null) {
+            self::$identifiers = (new Set('string'))->add('float');
+        }
+
+        return self::$identifiers;
+    }
+}
---
+++ Tests/Definition/Type/FloatTypeTest.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Rest\Server\Tests\Definition\Type;
+
+use Innmind\Rest\Server\Definition\{
+    Type\FloatType,
+    TypeInterface
+};
+use Innmind\Immutable\Collection;
+
+class FloatTypeTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(TypeInterface::class, new FloatType);
+        $this->assertSame(
+            ['float'],
+            FloatType::identifiers()->toPrimitive()
+        );
+        $this->assertInstanceOf(
+            FloatType::class,
+            FloatType::fromConfig(new Collection([]))
+        );
+    }
+
+    public function testDenormalize()
+    {
+        $this->assertSame(
+            42.0,
+            (new FloatType)->denormalize(42.0)
+        );
+    }
+
+    /**
+     * @expectedException Innmind\Rest\Server\Exception\DenormalizationException
+     * @expectedExceptionMessage The value must be a float
+     */
+    public function testThrowWhenNotDenormalizingAString()
+    {
+        (new FloatType)->denormalize(42);
+    }
+
+    public function testNormalize()
+    {
+        $this->assertSame(
+            42.0,
+            (new FloatType)->normalize(42.0)
+        );
+    }
+
+    /**
+     * @expectedException Innmind\Rest\Server\Exception\NormalizationException
+     * @expectedExceptionMessage The value must be a float
+     */
+    public function testThrowWhenNotNormalizingAString()
+    {
+        (new FloatType)->normalize(42);
+    }
+}
