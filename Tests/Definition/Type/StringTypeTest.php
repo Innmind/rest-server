b9d84670f45f20aed33dffac7c1436737d3bd4a8
Baptiste Langlade <baptouuuu@gmail.com>
2016-04-09 16:22:22 +0200

    add string type

---
+++ Tests/Definition/Type/StringTypeTest.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Rest\Server\Tests\Definition\Type;
+
+use Innmind\Rest\Server\Definition\{
+    Type\StringType,
+    TypeInterface
+};
+use Innmind\Immutable\Collection;
+
+class StringTypeTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(TypeInterface::class, new StringType);
+        $this->assertSame(
+            ['string'],
+            StringType::identifiers()->toPrimitive()
+        );
+        $this->assertInstanceOf(
+            StringType::class,
+            StringType::fromConfig(new Collection([]))
+        );
+    }
+
+    public function testDenormalize()
+    {
+        $this->assertSame(
+            'foo',
+            (new StringType)->denormalize('foo')
+        );
+    }
+
+    /**
+     * @expectedException Innmind\Rest\Server\Exception\DenormalizationException
+     * @expectedExceptionMessage The value must be a string
+     */
+    public function testThrowWhenNotDenormalizingAString()
+    {
+        (new StringType)->denormalize(42);
+    }
+
+    public function testNormalize()
+    {
+        $this->assertSame(
+            'foo',
+            (new StringType)->normalize('foo')
+        );
+    }
+
+    /**
+     * @expectedException Innmind\Rest\Server\Exception\NormalizationException
+     * @expectedExceptionMessage The value must be a string
+     */
+    public function testThrowWhenNotNormalizingAString()
+    {
+        (new StringType)->normalize(new class {
+            public function __toString()
+            {
+                return 'foo';
+            }
+        });
+    }
+}
