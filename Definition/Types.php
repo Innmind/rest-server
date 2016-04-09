2fb970d9d4c6bd5701bebb18a2fcff3e3931fb2e
Baptiste Langlade <baptouuuu@gmail.com>
2016-04-09 16:46:21 +0200

    add definition types

---
+++ Definition/Types.php
@@ -0,0 +1,99 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Rest\Server\Definition;
+
+use Innmind\Rest\Server\{
+    Definition\Type\ArrayType,
+    Definition\Type\SetType,
+    Definition\Type\BoolType,
+    Definition\Type\DateType,
+    Definition\Type\FloatType,
+    Definition\Type\IntType,
+    Definition\Type\StringType,
+    Exception\InvalidArgumentException
+};
+use Innmind\Immutable\{
+    Map,
+    MapInterface,
+    CollectionInterface
+};
+
+class Types
+{
+    private $types;
+
+    public function __construct()
+    {
+        $defaults = [
+            ArrayType::class,
+            BoolType::class,
+            DateType::class,
+            FloatType::class,
+            IntType::class,
+            StringType::class,
+        ];
+        $this->types = (new Map('string', 'string'));
+
+        foreach ($defaults as $default) {
+            $this->register($default);
+        }
+    }
+
+    /**
+     * Register the given type
+     *
+     * @param string $type FQCN
+     *
+     * @return self
+     */
+    public function register(string $type): self
+    {
+        $refl = new \ReflectionClass($type);
+
+        if (!$refl->implementsInterface(TypeInterface::class)) {
+            throw new InvalidArgumentException(sprintf(
+                'The type "%s" must implement TypeInterface',
+                $type
+            ));
+        }
+
+        call_user_func([$type, 'identifiers'])
+            ->foreach(function(string $identifier) use ($type) {
+                $this->types = $this->types->put(
+                    $identifier,
+                    $type
+                );
+            });
+
+        return $this;
+    }
+
+    /**
+     * Return the types mapping
+     *
+     * @return MapInterface<string, string>
+     */
+    public function all(): MapInterface
+    {
+        return $this->types;
+    }
+
+    /**
+     * Build a new type instance of the wished type
+     *
+     * @param string $type
+     * @param CollectionInterface $config
+     *
+     * @return TypeInterface
+     */
+    public function build(string $type, CollectionInterface $config): TypeInterface
+    {
+        $config = $config->set('_types', $this);
+
+        return call_user_func(
+            [$this->types->get($type), 'fromConfig'],
+            $config
+        );
+    }
+}
---
+++ Tests/Definition/TypesTest.php
@@ -0,0 +1,77 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Rest\Server\Tests;
+
+use Innmind\Rest\Server\Definition\{
+    Types,
+    TypeInterface,
+    Type\ArrayType,
+    Type\BoolType,
+    Type\DateType,
+    Type\FloatType,
+    Type\IntType,
+    Type\StringType
+};
+use Innmind\Immutable\{
+    Set,
+    Collection
+};
+
+class TypesTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAll()
+    {
+        $t = new Types;
+
+        $this->assertSame(
+            [
+                'array',
+                'bool',
+                'boolean',
+                'date',
+                'float',
+                'int',
+                'integer',
+                'string',
+            ],
+            $t->all()->keys()->toPrimitive()
+        );
+        $this->assertSame(
+            [
+                ArrayType::class,
+                BoolType::class,
+                BoolType::class,
+                DateType::class,
+                FloatType::class,
+                IntType::class,
+                IntType::class,
+                StringType::class,
+            ],
+            $t->all()->values()->toPrimitive()
+        );
+    }
+
+    /**
+     * @expectedException Innmind\Rest\Server\Exception\InvalidArgumentException
+     * @expectedExceptionMessage The type "stdClass" must implement TypeInterface
+     */
+    public function testThrowWhenRegisteringingInvalidType()
+    {
+        (new Types)->register('stdClass');
+    }
+
+    public function testBuild()
+    {
+        $t = new Types;
+
+        $this->assertInstanceOf(
+            StringType::class,
+            $t->build('string', new Collection([]))
+        );
+        $this->assertInstanceOf(
+            ArrayType::class,
+            $t->build('array', new Collection(['inner' => 'string']))
+        );
+    }
+}
