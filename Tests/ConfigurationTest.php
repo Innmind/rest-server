bf53a0287ff07ba207acdb834fd56e563404b4e8
Baptiste Langlade <baptouuuu@gmail.com>
2016-04-14 08:22:41 +0200

    add configuration

---
+++ Tests/ConfigurationTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Rest\Server\Tests;
+
+use Innmind\Rest\Server\Configuration;
+use Symfony\Component\{
+    Config\Definition\Processor,
+    Yaml\Yaml
+};
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testProcess()
+    {
+        $c = new Configuration;
+        $p = new Processor;
+
+        $result = $p->processConfiguration(
+            $c,
+            [$expected = Yaml::parse(file_get_contents('fixtures/mapping.yml'))]
+        );
+
+        $expected['top_dir']['resources']['image']['properties']['uuid']['access'] = ['READ'];
+        $expected['top_dir']['resources']['image']['properties']['uuid']['variants'] = [];
+        $expected['top_dir']['resources']['image']['properties']['uuid']['optional'] = false;
+        $expected['top_dir']['resources']['image']['properties']['url']['variants'] = [];
+        $expected['top_dir']['resources']['image']['properties']['url']['optional'] = false;
+        $expected['top_dir']['resources']['image']['options'] = [];
+        $expected['top_dir']['resources']['image']['metas'] = [];
+
+        $this->assertSame($expected, $result);
+    }
+}
