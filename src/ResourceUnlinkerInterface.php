d9cee33eccd63991af37f90793f243f1a3aa0093
Baptiste Langlade <baptouuuu@gmail.com>
2016-07-12 08:09:25 +0200

    change the linker and unlinker signatures

--- src/ResourceUnlinkerInterface.php
+++ src/ResourceUnlinkerInterface.php
@@ -3,13 +3,16 @@ declare(strict_types = 1);
 
 namespace Innmind\Rest\Server;
 
+use Innmind\Immutable\MapInterface;
+
 interface ResourceUnlinkerInterface
 {
     /**
+     * @param Reference $from
+     * @param MapInterface<Reference, MapInterface<string, ParameterInterface>> $tos
+     *     All relationships must be removed atomically (all or nothing)
+     *
      * @return void
      */
-    public function __invoke(
-        HttpResourceInterface $from,
-        HttpResourceInterface $to
-    );
+    public function __invoke(Reference $from, MapInterface $tos);
 }
